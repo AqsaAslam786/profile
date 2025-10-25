d23d9d2c0ff558d96413dae543a20517261175f4
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-25 08:11:39 +0100

    save

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  
+
+  return (
+    <>
+      <UserProfile 
+      
+      name='Aqsa'
+      image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYqoKTu_o3Zns2yExbst2Co84Gpc2Q1RJbA&s'
+      description='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen'
+      />
+    </>
+  )
+}
+
+export default App
